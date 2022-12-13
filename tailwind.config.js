a31b6ac8d66e81aa5b48562089a7b458df6a7f03
lennoxstark47 <twisamhaldar@gmail.com>
2022-12-13 21:16:48 +0530

    completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    // colors: {
+    //   'buttonBlueDark': '#3B66B8'
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
